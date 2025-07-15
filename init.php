49cd1017e64a39062058a2af40b6a654ee52de3f
mk6742 <elciaderutima8460@docomo.ne.jp>
2025-07-15 13:05:53 +0900

    修正

--- init.php
+++ init.php
@@ -6,7 +6,7 @@ require_once("cURL.php");
 $curlclass = new cURLClass();
 
 $URL    = 'https://app.nexus-fms.com';
-$ID        = 'masaaki.kera';
-$PW        = '8011628';
+$ID        = '';
+$PW        = '';
 $DB        = 'ASLESx'; // 必要に応じてカスタム App のファイル名(データベース名)を指定
 $AUTH    = base64_encode($ID . ':' . $PW);
